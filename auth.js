fe09732508839f4fcbff97375c4436ca7b8c5665
omarr01116 <r01116d@ms.upla.edu.pe>
2025-10-12 12:37:03 -0500

    sgfsd

--- auth.js
+++ auth.js
@@ -7,7 +7,7 @@ const SUPABASE_URL = 'https://bazwwhwjruwgyfomyttp.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJhend3aHdqcnV3Z3lmb215dHRwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgxNjA1NTAsImV4cCI6MjA3MzczNjU1MH0.RzpCKpYV-GqNIhTklsQtRqyiPCGGmVlUs7q_BeBHxUo'; 
 
 // 🔥 URL del Backend Local (Para obtener el rol)
-const BACKEND_URL = "http://localhost:3000/api/login"; 
+const BACKEND_URL = "https://trabajo-backend.onrender.com/api/login"; 
 
 // URLs para la redirección post-OAuth
 const LOCAL_REDIRECT = "http://127.0.0.1:5500/pagina/login.html"; 
